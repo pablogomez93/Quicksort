96031298f1809af91491f3d966c7101e9fe9dbf6
Pablo Gómez <pagomez@despegar.com>
2015-07-21 14:13:57 -0300

    Added charge of numbers to apply Quick Sort as a console parameters, but, for now, that feature only works partially

--- quicksort.c
+++ quicksort.c
@@ -2,6 +2,13 @@
 
 typedef unsigned int uint;
 
+void printCollection(int* collection, uint n){
+    int i;
+    for (i = 0; i < n; ++i)
+        printf("%d ", collection[i]);
+    printf("\n");
+}
+
 void QuickSort(int* array, uint n) {
     if(n < 2) return;
 
@@ -33,11 +40,24 @@ void QuickSort(int* array, uint n) {
 }
 
 
-int main() {
+int main(int argsSize, char* args[]) {
     int array[5] = {8137, 3522, 7383, 7336, 9659};
     int n = sizeof array / sizeof array[0];
+    int i;
+
+    if(argsSize > 1){
+        n = argsSize-1;
+        array[n];
+
+        int i;
+        for (i = 1; i <= n; ++i)
+            array[i-1] = *args[i] - '0';
+
+    }
 
     QuickSort(array, n);
+
+    printCollection(array, n);
 
 	return 0;
 }
