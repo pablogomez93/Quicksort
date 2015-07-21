81dc6ea3d6144e561a5c088d337e8721455506f7
Mago <mago-1986@hotmail.com>
2015-07-21 17:14:41 -0300

    Added comments to the main function to explain the code there

--- quicksort.c
+++ quicksort.c
@@ -41,11 +41,27 @@ void QuickSort(int* array, uint n) {
 
 
 int main(int argsSize, char* args[]) {
+	/* 
+	 * Charging the array to sort.
+	 * Define n as total size of the array and divide it by the size of one
+	 * element of it. It's a kind of hack to find the length of the array.
+	 */
     int array[5] = {8137, 3522, 7383, 7336, 9659};
     int n = sizeof array / sizeof array[0];
-    int i;
 
-    if(argsSize > 1){
+	/*
+	 * If you prefer, you can charge the collection to sort into the program
+	 * by passing its elements as arguments from a terminal.
+	 * If you don't pass arguments to sort, this program will sort the array
+	 * charged manually above.
+	 *
+	 * Example: 
+	 *	$ make all
+	 *  $ ./quicksort 5 7 8 6 69 18782 9
+	 *
+	 *  Then Quick Sort will sort the array [5, 7, 8, 6, 69, 18782, 9] 
+	 */
+	if(argsSize > 1) {
         n = argsSize-1;
         array[n];
 
@@ -55,8 +71,10 @@ int main(int argsSize, char* args[]) {
 
     }
 
+    //Finally, sort the collection with Quick Sort Algorithm
     QuickSort(array, n);
 
+    //Send to the standar output the sorted array 
     printCollection(array, n);
 
 	return 0;
